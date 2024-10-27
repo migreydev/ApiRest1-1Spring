70ddfa7e95c5f303db410685604f9740eee142eb
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2024-10-27 13:20:47 +0100

    añadiendo proyecto

---
+++ src/main/java/com/vedruna/football/Football11Application.java
@@ -0,0 +1,13 @@
+package com.vedruna.football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Football11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Football11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedruna/football/Football11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedruna.football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Football11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
